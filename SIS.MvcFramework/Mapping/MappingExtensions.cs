2f93388d295f4610536484116789927c47a08988
Patrick <richardblake@abv.bg>
2019-12-08 11:20:56 +0300

    ModelMapper.cs modified - recursive, works with IEnumerable collections

--- SIS.MvcFramework/Mapping/MappingExtensions.cs
+++ SIS.MvcFramework/Mapping/MappingExtensions.cs
@@ -5,7 +5,7 @@ namespace SIS.MvcFramework.Mapping
 {
     public static class MappingExtensions
     {
-        public static IEnumerable<TDestination> To<TDestination>(this List<object> collection)
+        public static IEnumerable<TDestination> To<TDestination>(this IEnumerable<object> collection)
         {
             return collection.Select(ModelMapper.ProjectTo<TDestination>).ToList();
         }
--- SIS.MvcFramework/Mapping/ModelMapper.cs
+++ SIS.MvcFramework/Mapping/ModelMapper.cs
@@ -1,33 +1,68 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 
 namespace SIS.MvcFramework.Mapping
 {
-    public class ModelMapper
+    public static class ModelMapper
     {
         public static TDestination ProjectTo<TDestination>(object origin)
         {
-            TDestination destinationInstance = (TDestination)Activator.CreateInstance(typeof(TDestination));
+            return (TDestination)MapObject(origin, typeof(TDestination));
+        }
+
+        private static object MapObject(object origin, Type declarationType)
+        {
+            object destinationInstance = Activator.CreateInstance(declarationType);
 
             foreach (PropertyInfo originProperty in origin.GetType().GetProperties())
             {
                 string propertyName = originProperty.Name;
                 PropertyInfo destinationProperty = destinationInstance.GetType().GetProperty(propertyName);
 
-                if (destinationProperty != null)
-                {
-                    if (destinationProperty.PropertyType == typeof(string))
-                    {
-                        destinationProperty.SetValue(destinationInstance, originProperty.GetValue(origin).ToString());
-                    }
-                    else
-                    {
-                        destinationProperty.SetValue(destinationInstance, originProperty.GetValue(origin));
-                    }
-                }
+                MapProperty(origin, destinationInstance, originProperty, destinationProperty);
             }
 
             return destinationInstance;
+        }
+
+        private static void MapProperty(object origin, object destinationInstance, PropertyInfo originProperty, PropertyInfo destinationProperty)
+        {
+            if (destinationProperty == null)
+            {
+                return;
+            }
+
+            object originPropertyValue = originProperty.GetValue(origin);
+
+            if (destinationProperty.PropertyType == typeof(string) || destinationProperty.PropertyType == typeof(decimal))
+            {
+                destinationProperty.SetValue(destinationInstance, originProperty.GetValue(origin));
+            }
+            else if (destinationProperty.PropertyType.GetInterfaces().Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IEnumerable<>)))
+            {
+
+                IEnumerable originCollection = (IEnumerable)originProperty.GetValue(origin);
+                IList destinationCollection = (IList)Activator.CreateInstance(destinationProperty.PropertyType);
+
+                Type destinationElementType = destinationProperty.GetValue(destinationInstance).GetType().GetGenericArguments()[0];
+
+                foreach (object originElement in originCollection)
+                {
+                    destinationCollection.Add(MapObject(originElement, destinationElementType));
+                }
+
+                destinationProperty.SetValue(destinationInstance, destinationCollection);
+            }
+            else
+            {
+                // Not tested.
+                object value = MapObject(originProperty.GetValue(origin), destinationProperty.GetValue(destinationInstance).GetType());
+
+                destinationProperty.SetValue(destinationInstance, value);
+            }
         }
     }
 }
